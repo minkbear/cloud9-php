d3b3cf9eaf0d1b987d87b3336a3fe8b08a8d14a8
Minkbear <minkbear@gmail.com>
2024-02-09 14:07:23 +0700

    Create prepare-php.sh

---
+++ prepare-php.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+# Generate SSH
+[[ ! -f ~/.ssh/id_rsa ]] && ssh-keygen -f ~/.ssh/id_rsa -N ""
+
+# Configure VIM
+cat <<'EOF' > ~/.vimrc
+source /usr/share/vim/vim82/defaults.vim
+set mouse=
+EOF
+
+# Create .local/bin
+# mkdir -p $HOME/.local/bin
+
+# Install Docker Compose v2
+export DOCKER_COMPOSE_DLVERSION=v2.24.2
+mkdir -p ~/.docker/cli-plugins/
+curl -SL https://github.com/docker/compose/releases/download/$DOCKER_COMPOSE_DLVERSION/docker-compose-linux-x86_64 -o ~/.docker/cli-plugins/docker-compose
+chmod +x ~/.docker/cli-plugins/docker-compose
+docker compose version
+
+# Install Composer 2
+cd ~
+curl -s https://getcomposer.org/installer | php
+# move composer into a bin directory you control:
+sudo mv composer.phar /usr/local/bin/composer
+composer about
